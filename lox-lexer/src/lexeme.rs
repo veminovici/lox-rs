5b31d460f3b28995fe8d05d35d8f78e19fc95576
veminovici <veminovici@hotmail.com>
2021-11-27 00:02:51 +0200

    Add more test

--- lox-lexer/src/lexeme.rs
+++ lox-lexer/src/lexeme.rs
@@ -204,3 +204,226 @@ impl Display for Lexeme {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Lexeme;
+
+    fn test_lexeme(l: Lexeme) {
+        let s = format!("{}", l);
+        assert!(!s.is_empty());
+
+        let s = format!("{:?}", l);
+        assert!(!s.is_empty());
+    }
+
+    #[test]
+    fn test_debug_left_parenthesis() {
+        test_lexeme(Lexeme::LeftParen);
+    }
+
+    #[test]
+    fn test_debug_right_parenthesis() {
+        test_lexeme(Lexeme::RightParen);
+    }
+
+    #[test]
+    fn test_debug_left_brace() {
+        test_lexeme(Lexeme::LeftBrace);
+    }
+
+    #[test]
+    fn test_debug_right_brace() {
+        test_lexeme(Lexeme::RightBrace);
+    }
+
+    #[test]
+    fn test_debug_comma() {
+        test_lexeme(Lexeme::Comma);
+    }
+
+    #[test]
+    fn test_debug_dot() {
+        test_lexeme(Lexeme::Dot);
+    }
+
+    #[test]
+    fn test_debug_plus() {
+        test_lexeme(Lexeme::Plus);
+    }
+
+    #[test]
+    fn test_debug_minus() {
+        test_lexeme(Lexeme::Minus);
+    }
+
+    #[test]
+    fn test_debug_semicolon() {
+        test_lexeme(Lexeme::Semicolon);
+    }
+
+    #[test]
+    fn test_debug_slash() {
+        test_lexeme(Lexeme::Slash);
+    }
+
+    #[test]
+    fn test_debug_star() {
+        test_lexeme(Lexeme::Star);
+    }
+
+    #[test]
+    fn test_debug_bang() {
+        test_lexeme(Lexeme::Bang);
+    }
+
+    #[test]
+    fn test_debug_bang_equal() {
+        test_lexeme(Lexeme::BangEqual);
+    }
+
+    #[test]
+    fn test_debug_equal() {
+        test_lexeme(Lexeme::Equal);
+    }
+
+    #[test]
+    fn test_debug_equal_equal() {
+        test_lexeme(Lexeme::EqualEqual);
+    }
+
+    #[test]
+    fn test_debug_greater() {
+        test_lexeme(Lexeme::Greater);
+    }
+
+    #[test]
+    fn test_debug_greater_equal() {
+        test_lexeme(Lexeme::GreaterEqual);
+    }
+
+    #[test]
+    fn test_debug_less() {
+        test_lexeme(Lexeme::Less);
+    }
+
+    #[test]
+    fn test_debug_less_equal() {
+        test_lexeme(Lexeme::LessEqual);
+    }
+
+    #[test]
+    fn test_debug_identifier() {
+        test_lexeme(Lexeme::Identifier("hello".to_string()));
+    }
+
+    #[test]
+    fn test_debug_string() {
+        test_lexeme(Lexeme::String("hello".to_string()));
+    }
+
+    #[test]
+    fn test_debug_comment() {
+        test_lexeme(Lexeme::Comment("hello".to_string()));
+    }
+
+    #[test]
+    fn test_debug_number() {
+        test_lexeme(Lexeme::Number(12.3));
+    }
+
+    #[test]
+    fn test_debug_and() {
+        test_lexeme(Lexeme::And);
+    }
+
+    #[test]
+    fn test_debug_class() {
+        test_lexeme(Lexeme::Class);
+    }
+
+    #[test]
+    fn test_debug_else() {
+        test_lexeme(Lexeme::Else);
+    }
+
+    #[test]
+    fn test_debug_false() {
+        test_lexeme(Lexeme::False);
+    }
+
+    #[test]
+    fn test_debug_for() {
+        test_lexeme(Lexeme::For);
+    }
+
+    #[test]
+    fn test_debug_fun() {
+        test_lexeme(Lexeme::Fun);
+    }
+
+    #[test]
+    fn test_debug_if() {
+        test_lexeme(Lexeme::If);
+    }
+
+    #[test]
+    fn test_debug_nil() {
+        test_lexeme(Lexeme::Nil);
+    }
+
+    #[test]
+    fn test_debug_or() {
+        test_lexeme(Lexeme::Or);
+    }
+
+    #[test]
+    fn test_debug_print() {
+        test_lexeme(Lexeme::Print);
+    }
+
+    #[test]
+    fn test_debug_return() {
+        test_lexeme(Lexeme::Return);
+    }
+
+    #[test]
+    fn test_debug_super() {
+        test_lexeme(Lexeme::Super);
+    }
+
+    #[test]
+    fn test_debug_this() {
+        test_lexeme(Lexeme::This);
+    }
+
+    #[test]
+    fn test_debug_true() {
+        test_lexeme(Lexeme::True);
+    }
+
+    #[test]
+    fn test_debug_var() {
+        test_lexeme(Lexeme::Var);
+    }
+
+    #[test]
+    fn test_debug_while() {
+        test_lexeme(Lexeme::While);
+    }
+
+    #[test]
+    fn test_debug_whitespace() {
+        test_lexeme(Lexeme::Whitespace("  ".to_string()));
+    }
+
+    #[test]
+    fn test_debug_newline() {
+        test_lexeme(Lexeme::NewLine);
+    }
+
+    #[test]
+    fn test_debug_eof() {
+        test_lexeme(Lexeme::Eof);
+    }
+}
